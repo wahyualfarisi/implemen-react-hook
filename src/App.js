50f4090f421394ba6daa413ed5673eae429743ca
wahyu alfarisi <wahyualfarisi@gmail.com>
2020-09-13 18:49:06 +0700

    using Context with UseContext Hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Ingredients from './components/Ingredients/Ingredients';
+import Auth from './components/Auth';
+import { AuthContext } from './context/auth-context';
 
 const App = props => {
-  return <Ingredients />;
+  const authcontext = useContext(AuthContext);
+
+  let content = <Auth />
+  if(authcontext.isAuth){
+    content = <Ingredients />
+  }
+
+  return content;
 };
 
 export default App;
